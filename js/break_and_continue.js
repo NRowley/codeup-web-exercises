bb1ba70582259bdbb9bae4258ca94d31ec6e287b
Nicholas Rowley <nicholas.ryan.rowley@gmail.com>
2023-06-05 16:56:27 -0500

    commit iterating_arrays_js.html and js/iterating.js, modified Loop_Practice.html and js/break_and_continue.js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -3,11 +3,20 @@ console.log('Break and Continue Exercise');
 let oddNum = parseInt(prompt("please enter an ODD number between 1 and 50"));
 console.log(oddNum);
 
-while(oddNum % 2 === 0){
-    oddNum = parseInt(prompt(`I said enter an ODD number between 1 and 50`));
-    console.log(oddNum);
-    break;
+for (let i = 1; i < 100; i++) {
+    if (oddNum % 2 != 0 && oddNum >= 1 && oddNum <= 49) {
+        console.log(`${oddNum} is your number`);
+        break;
+    } else {
+        oddNum = parseInt(prompt(`Attempt number: ${i}... I said enter an ODD number between 1 and 50`));
+    }
 }
+
+// while(oddNum % 2 === 0){
+//     oddNum = parseInt(prompt(`I said enter an ODD number between 1 and 50`));
+//     console.log(oddNum);
+//     break;
+// }
 
 for (let i = 1; i < 50; i++) {
     if (i === oddNum) {
