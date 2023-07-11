dec36d53bdb32178638c06e6c3282559e4b8bddf
Nicholas Rowley <nicholas.ryan.rowley@gmail.com>
2023-07-11 09:42:57 -0500

    konami exercise files

--- js/konami.js
+++ js/konami.js
@@ -8,18 +8,27 @@
 let game = {
     playerLives: ($("#lives").children()).length,
     iconPresent: false,
-    input: false
+    success: false,
+    start: false
 };
 const up = 38;
 const down = 40;
 const left = 37;
 const right = 39;
 
-let iconDirections = [up, down, left, right];
-let konamiCodeInput = [];
-let konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+let iconDirections = ["&larr", "&uarr", "&darr", "&rarr"];
+// let konamiCodeInput = [];
+// let konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 let generateIconsIntervalId;
 
+let gameOver = function (){
+    let gameOverConfirm = confirm("Play Again?");
+    if(gameOverConfirm){
+        location.reload();
+    } else {
+        $("body").html("THANKS FOR PLAYING");
+    }
+}
 //Game functions
 let inputGenerator = function () {
     const direction = Math.floor(Math.random() * (iconDirections.length));
@@ -31,17 +40,11 @@ let inputGenerator = function () {
         $('.game-icon').html("&uarr;");
     } else if (direction === 2) {
         $('.game-icon').html("&darr;");
-    } else if (direction === 3)
-    {
+    } else if (direction === 3) {
         $('.game-icon').html("&rarr;");
     }
+    return direction;
 }
-
-let inputChecker = function (key) {
-    if (key === 0) {
-        console.log(`success`);
-    }
-};
 let addHearts = function () {
     for (let i = 0; i < 30; i++) {
         $("#lives").append("<span class='heart'>&#10084;<span>");
@@ -79,46 +82,67 @@ let generateIcons = function () {
         let newWidth = `${parseInt($('.game-icon').css('width')) - 5}px`;
         $('.game-icon').css('height', newHeight);
         $('.game-icon').css('width', newWidth);
-        if (parseInt(newHeight) === 75) {
-            $('.game-icon').css('background-color', 'orange');
-        } else if (parseInt(newHeight) === 60) {
-            $('.game-icon').css('background-color', 'red');
-        } else if (parseInt(newHeight) === 35) {
-            $('.game-area').html("");
-            game.iconPresent = false;
-            $('.heart').last().remove();
-            if (game.playerLives === 0) {
-                confirm(`GAME OVER... Continue?`);
-                clearInterval(generateIconsIntervalId);
+        if(game.success){
+            $('.game-icon').css('background-color', 'blue');
+            $('.game-icon').css('color', 'white');
+            $('.game-icon').html("SUCCESS");
+            clearInterval(generateIconsIntervalId);
+            setTimeout(gameOver(), 1000);
+        }
+
+        if(!game.success){
+            if (parseInt(newHeight) === 75) {
+                $('.game-icon').css('background-color', 'orange');
+            } else if (parseInt(newHeight) === 60) {
+                $('.game-icon').css('background-color', 'red');
+            } else if (parseInt(newHeight) === 35) {
+                $('.game-area').html("");
+                game.iconPresent = false;
+                $('.heart').last().remove();
+                if (game.playerLives === 0) {
+                    gameOver();
+                    clearInterval(generateIconsIntervalId);
+                }
+                game.playerLives--;
             }
-            game.playerLives--;
         }
     }
 };
 
-$(document).keyup(function (event) {
-    console.log(event.keyCode);
-    konamiCodeInput.push(event.keyCode);
-    console.log(`Code Input: ${konamiCodeInput}`);
-    let i = 0;
-    if (konamiCodeInput[i] != konamiCode[i]) {
-        konamiCodeInput = [];
-    } else if (konamiCodeInput.toString() === konamiCode.toString()) {
-        alert(`Success! Extra-lives +30!`);
-        addHearts();
-    }
-    if (event.keyCode === 32) {
-        startBtn();
-    }
-});
-//check key input
-$(document).keyup(function (e) {
-    if (game.input === false) {
-
-    }
-})
-// $('#start').on('click', function (){
-//     startBtn();
+//code is in html for exercise
+// $(document).keyup(function (event) {
+//     console.log(event.keyCode);
+//     konamiCodeInput.push(event.keyCode);
+//     console.log(`Code Input: ${konamiCodeInput}`);
+//     if(game.start) {
+//         let input;
+//         if (event.keyCode === 38) {
+//             input = "&uarr";
+//         } else if (event.keyCode === 40) {
+//             input = "&darr";
+//         } else if (event.keyCode === 37) {
+//             input = "&larr";
+//         } else if (event.keyCode === 39) {
+//             input = "&rarr";
+//         }
+//         if (input === iconDirections[inputGenerator()]) {
+//             $('.game-icon').css('background-color', 'blue');
+//             game.success = true;
+//         }
+//
+//     } else if (!game.start){
+//         let i = 0;
+//         if (konamiCodeInput[i] != konamiCode[i]) {
+//             konamiCodeInput = [];
+//         } else if (konamiCodeInput.toString() === konamiCode.toString()) {
+//             alert(`Success! Extra-lives +30!`);
+//             addHearts();
+//         }
+//         if (event.keyCode === 32) {
+//             game.start = true;
+//             startBtn();
+//         }
+//     }
 // });
 
 
