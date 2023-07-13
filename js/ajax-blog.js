5bcd3071857b333310aa7216b51ca71cbd30ea6f
Nicholas Rowley <nicholas.ryan.rowley@gmail.com>
2023-07-13 13:44:51 -0500

    commit .gitignore added key.js. ajax exercises for inventory and blog

---
+++ js/ajax-blog.js
@@ -0,0 +1,24 @@
+"use strict"
+const blogPosts = document.querySelector('#posts')
+$.ajax('data/blog.json').done((response)=>{
+    console.log(`getting blog posts`);
+    console.log(response);
+    renderBlogPosts(response);
+})
+
+function renderBlogPosts(posts){
+    console.log('rendering blog posts...')
+    for(let i = 0; i < posts.length; i++){
+        console.log(posts[i].categories);
+        blogPosts.innerHTML += (`
+        <div class="card product-card" style="width: 18rem;">
+                    <div class="card-body post-card">
+                    <h5 class="card-title">${posts[i].title}</h5>
+                    <p class="card-text">${posts[i].content}</p>
+                    <p class="card-text"><p>Categories: ${posts[i].categories.join(' ')}</p><p>Date Posted: ${posts[i].date}</p></p>
+                   
+                    </div>
+                </div>
+        `)
+    }
+}
---
+++ js/ajax-store.js
@@ -0,0 +1,43 @@
+(function () {
+    "use strict";
+    const productInventory = document.querySelector('#insertProducts');
+    const refreshInventoryBtn = document.querySelector('#refresh-inventory');
+
+    // TODO: Create an AJAX GET request for the file under data/inventory.json
+    $.ajax('data/inventory.json').done((response) => {
+        console.log(response);
+        renderInventory(response);
+    });
+
+    //Why does the refresh fuck up my formatting???----------------
+    let refreshInventory = function () {
+        $.ajax('data/inventory.json').done((response) => {
+            console.log('Refresh Inventory');
+            productInventory.innerHTML = "";
+            renderInventory(response);
+        })
+    }
+
+    // TODO: Take the data from inventory.json and append it to the products table
+    //       HINT: Your data should come back as a JSON object; use console.log() to inspect
+    //             its contents and fields
+    //       HINT: You will want to target #insertProducts for your new HTML elements
+    function renderInventory(products) {
+        for (let i = 0; i < products.length; i++) {
+            productInventory.innerHTML += (`
+                <div class="card product-card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">${products[i].title}</h5>
+                    <p class="card-text"><span>$${products[i].price}$</span><span>${products[i].quantity} in stock</span></span></p>
+                    <p class="card-text">${products[i].categories.join(' ')}</p>
+                    <a href="#" class="btn btn-primary">Add to shopping cart</a>
+                    </div>
+                </div>
+
+                `)
+        }
+    }
+
+    refreshInventoryBtn.addEventListener('click', (refreshInventory));
+})();
