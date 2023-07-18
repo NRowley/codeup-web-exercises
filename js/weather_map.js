16357fae11b1fe1e0798bff509315f760c561dc2
Nicholas Rowley <nicholas.ryan.rowley@gmail.com>
2023-07-18 00:25:56 -0500

    weather map files

--- js/weather_map.js
+++ js/weather_map.js
@@ -2,79 +2,252 @@
 //GLOBAL VARIABLES AND QUERIES////////////////
 //MAPBOX
 mapboxgl.accessToken = MAPBOX_TOKEN;
-const map = new mapboxgl.Map({
+let map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/outdoors-v12',
     zoom: 1,
     center: [8.25069, 49.99234]
 });
+let searchAddress;
+let marker;
+let addressHistory = [];
+let savedAddresses = [];
 
 //OPEN WEATHER
 const OPEN_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/forecast';
 let currentWeatherData;
+let weatherURL;
+let dateFormat;
+let localAvgTemp = [];
+let avgTemp;
+
 //QUERIES
 const forecastCards = document.querySelector('.forecast-cards');
-const forecastHeader = document.querySelector('.forecast-header');
-//Subject to change...
-const SAN_ANTONIO_COORD = ['29.4260', '-98.4861'];
-console.log(...SAN_ANTONIO_COORD);
-const URL = createWeatherURL(...SAN_ANTONIO_COORD);
+const forecastHeaderCity = document.querySelector('.forecast-header-city');
+const searchTextBox = document.querySelector('.search');
+const searchSubmitBtn = document.querySelector('#search-submit');
+const historyHead = document.querySelector('#history-head');
+const historyItems = document.querySelector('.history-col');
+const savedHead = document.querySelector('#saved-head');
+const savedItems = document.querySelector('.saved-col');
+let historyItemButtons = document.querySelectorAll('.history-item');
+let savedItemButtons = document.querySelectorAll('.saved-item');
+let favStar = document.querySelectorAll('.fav-star');
+const bodyBg = document.querySelector('body');
+const forecastHeaderWrapper = document.querySelector('.forecast-header-wrapper');
+const mapContainer = document.querySelector('.map-container');
 
+//OTHER VARIABLES
+const monthsArr = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+let cardOpen = false;
+let cardEvent;
+let hiddenConditions;
 
 
 //FUNCTIONS////////////////////////////
-function createWeatherURL(lat, lon) {
+//WEATHER FUNCTIONS
+function createWeatherURL(lon, lat) {
     return `${OPEN_WEATHER_URL}?lat=${lat}&lon=${lon}&units=imperial&appid=${OPEN_WEATHER_APPID}`;
 }
-
-function getWeatherData(url){
+function getWeatherData(url) {
     $.ajax(url).done(data => {
         currentWeatherData = data;
         console.log(currentWeatherData);
         getFiveDayForecast(data);
     }).fail(console.error);
 }
-
-function renderForecastCard(forecast){
-    console.log('rendering cards')
-    console.log(currentWeatherData);
-    forecastHeader.innerHTML = (`
+function renderForecastCard(forecast) {
+    calculateAvgTemp(localAvgTemp);
+    forecastHeaderWrapper.style.display = 'flex';
+    forecastHeaderCity.innerHTML = (`
         <h3 class="forecast-city">${currentWeatherData.city.name.toUpperCase()}</h3>
     `);
     forecastCards.innerHTML = "";
     forecast.forEach((day) => {
+        dateFormat = new Date(day.dt_txt);
+        let dayDate = dateFormat.getDate().toString();
+        let month = monthsArr[dateFormat.getMonth() - 1];
+        let year = dateFormat.getFullYear();
+        let sky = day.weather[0].description;
+
         forecastCards.innerHTML += (`
         <div class="day-forecast-card">
-        <h4 class="h2-day">Day: ${day.dt_txt}</h4>
-        <p class="conditions temp">${day.main.temp_max}/${day.main.temp_min}</p>
-        <p class="conditions">${day.weather[0].main}</p>
-        <p class="conditions">${day.weather[0].main}</p>
-        <p class="conditions">${day.weather[0].main}</p>
-        <p class="conditions">${day.weather[0].main}</p>
+        <h4 class="h4-day">${dayDate}${month}${year}</h4>
+        <p class="temp">${day.main.temp_max}&deg | ${day.main.temp_min}&deg</p>
+        <div class="hide">
+        <p class="conditions">Sky: ${day.weather[0].main}</p>
+        <p class="conditions">Humidity: ${day.main.humidity}</p>
+        <p class="conditions">Wind Speed: ${day.wind.speed}mph(?)</p>
+        <p class="conditions">Pressure: ${day.main.pressure}</p>
         </div>
-
+        </div>
     `)
     })
+
 }
-function getFiveDayForecast (data){
+function calculateAvgTemp(temps) {
+    let tempSum = temps[0] + temps [1] + temps [2] + temps [3] + temps [4];
+    avgTemp = tempSum / 5;
+    localAvgTemp = [];
+    if (avgTemp >= 100) {
+        bodyBg.style.backgroundColor = '#ce5b3e';
+    } else if (avgTemp >= 80) {
+        bodyBg.style.backgroundColor = '#e3964f';
+    } else if (avgTemp >= 65) {
+        bodyBg.style.backgroundColor = '#4ca273';
+    } else if (avgTemp >= 40) {
+        bodyBg.style.backgroundColor = '#4f7fe1';
+    } else if (avgTemp < 35) {
+        bodyBg.style.backgroundColor = '#74a1b2';
+    } else {
+        bodyBg.style.backgroundColor = 'black';
+    }
+}
+function getFiveDayForecast(data) {
     let forecast = [];
-    console.log(data.list);
-    for(let i = 0; i < data.list.length; i++){
-        if(i % 8 === 0){
+    // console.log(data.list);
+    for (let i = 0; i < data.list.length; i++) {
+        if (i % 8 === 0) {
             forecast.push(data.list[i]);
+            localAvgTemp.push(data.list[i].main.temp_max);
         }
     }
-    console.log(forecast);
+    console.log(localAvgTemp);
     renderForecastCard(forecast);
-    return(forecast);
+    return (forecast);
 }
 
+//MAPBOX FUNCTIONS
+function createMarker(address) {
+    geocode(address, MAPBOX_TOKEN).then(function (coordinates) {
+        searchAddress = {
+            coord: coordinates,
+            name: address,
+            info: ""
+        }
+        if (marker) {
+            marker.remove();
+        }
+        marker = new mapboxgl.Marker().setLngLat(searchAddress.coord).addTo(map);
+        goToPlace(searchAddress);
+    })
+}
+function renderAddressHistory(addresses) {
+    console.log('Rendering Address history')
+    historyItems.innerHTML = "";
+    addresses.forEach((address) => {
+        historyItems.innerHTML += (`
+        <div class="history-item"><span class="history-item-name">${address.name}</span><span class="fav-star">&starf;</span></div>
+        `)
+        historyItemButtons = document.querySelectorAll('.history-item');
+        favStar = document.querySelectorAll('.fav-star');
+        console.log(historyItemButtons);
+    })
+}
+function addToSaved(addresses) {
+    console.log('adding to saved items')
+    console.log(addresses);
+    savedItems.innerHTML = "";
+    addresses.forEach((address) => {
+        savedItems.innerHTML += (`
+        <div class="saved-item"><span class="saved-item-name">${address}</span></div>
+        `)
+        savedItemButtons = document.querySelectorAll('.saved-item');
+    })
+}
+function historyUpdate(place) {
+    if (addressHistory.length >= 10) {
+        console.log('remove last');
+        addressHistory.splice(9, 1);
+        addressHistory.unshift(place);
+    } else {
+        addressHistory.push(place);
+    }
+}
+function goToPlace(place) {
+    console.log('in goToPlace function');
+    map.setCenter(place.coord);
+    map.setZoom(5);
+    historyUpdate(place);
+    console.log(addressHistory);
+    weatherURL = createWeatherURL(...place.coord);
+    renderAddressHistory(addressHistory);
+    getWeatherData(weatherURL);
+}
 
 //EVENTS///////////////////////////////
-
+//Place marker by click...
+$(map).on('click', (event) => {
+    console.log(event.target);
+})
+searchSubmitBtn.addEventListener('click', (event) => {
+    map._marker = [];
+    createMarker(searchTextBox.value);
+})
+historyItems.addEventListener('click', (event) => {
+    console.log(event.target);
+    let target = event.target;
+    if (target.classList.contains('fav-star')) {
+        let historyName = target.previousElementSibling.innerHTML;
+        console.log(historyName);
+        if (savedAddresses.includes(historyName)) {
+            alert(`${historyName} is already in favorites!`);
+        } else {
+            // event.target.innerHTML = '&starf;';
+            event.target.style.color = 'gold';
+            savedAddresses.push(historyName);
+            addToSaved(savedAddresses);
+        }
+    } else if (target.classList.contains('history-item-name')) {
+        console.log('go to history');
+        let historyName = target.innerHTML;
+        console.log(historyName);
+        createMarker(historyName);
+    }
+})
+savedItems.addEventListener('click', (event) => {
+    if (event.target.classList.contains('saved-item-name')) {
+        createMarker(event.target.innerHTML);
+    }
+})
+forecastCards.addEventListener('mouseover', (event) => {
+    if (cardOpen === false) {
+        if (event.target.classList.contains('h4-day')) {
+            hiddenConditions = event.target.nextElementSibling.nextElementSibling;
+            cardEvent = event.target.parentElement;
+            cardEvent.style.transform = 'translateY(-2rem)';
+            cardEvent.style.backgroundColor = 'grey';
+            cardEvent.style.borderBottom = "unset";
+            hiddenConditions.style.display = 'unset';
+            cardOpen = true;
+        }
+    }
+})
+forecastCards.addEventListener('mouseleave', (event) => {
+    if (cardOpen) {
+        cardEvent.style.transform = 'unset';
+        cardEvent.style.backgroundColor = 'unset';
+        cardEvent.style.borderBottom = 'solid 3px white'
+        hiddenConditions.style.display = 'none';
+        cardOpen = false;
+    }
+})
+searchTextBox.addEventListener('keyup', (event) => {
+    console.log(event.keyCode);
+    if (event.keyCode === 13) {
+        createMarker(searchTextBox.value);
+    }
+})
+document.addEventListener('keyup', (event)=>{
+    console.log(event.key);
+    if(event.key === 'ArrowUp'){
+        map.setZoom(map.getZoom()+1);
+    } else if(event.key === 'ArrowDown'){
+        map.setZoom(map.getZoom()-1);
+    }
+})
 
 //RUN ON LOAD//////////////////////////
-getWeatherData(URL);
 
 //DEMO
 /**
